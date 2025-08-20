5d8d0481f01dc2fcf52455e461aa03f467b161fc
valshaden <valshaden@yandex.ru>
2025-08-20 12:38:11 +0300

    Сделано

--- 5.2.9. Convert the project into an executable file.py
+++ 5.2.9. Convert the project into an executable file.py
@@ -9,31 +9,33 @@
 # Напоминание
 # устанавливаем pygame C:\Python\Scripts\pip install pygame
 
+# Напоминание
+# устанавливаем pygame C:\Python\Scripts\pip install pygame
+
 from tkinter import *
 from tkinter import simpledialog as sd
 from tkinter import messagebox as mb
 import datetime
-import time
 import pygame
+import time
 
 t = None
+music = False  # Переменная для отслеживания проигрывания музыки
 
 def set():
-    global t
-    rem = sd.askstring("Время напоминания", "Введите время в формате ЧЧ:ММ (24-часовой формат)")
-    if rem:
+    global t, reminder_text
+    rem_time = sd.askstring("Время напоминания", "Введите время напоминания в формате ЧЧ:ММ (в 24-часовом формате):")
+    if rem_time:
         try:
-            hour = int(rem.split(":")[0])
-            minute = int(rem.split(":")[1])
+            hour = int(rem_time.split(":")[0])
+            minute = int(rem_time.split(":")[1])
             now = datetime.datetime.now()
-            print(now)
-            dt = now.replace(hour=hour, minute=minute)
-            print(dt)
+            dt = now.replace(hour=hour, minute=minute, second=0)
             t = dt.timestamp()
-            print(t)
-            label.config(text=f"Напоминание установлено на: {hour:02}:{minute:02}")
-        except ValueError:
-            mb.showerror("Ошибка", "Неверный формат времени")
+            reminder_text = sd.askstring("Текст напоминания", "Введите текст напоминания:")
+            label.config(text=f"Напоминание на {hour:02}:{minute:02} с текстом: {reminder_text}")
+        except Exception as e:
+            mb.showerror("Ошибка!", f"Произошла ошибка: {e}")
 
 def check():
     global t
@@ -45,9 +47,18 @@ def check():
     window.after(10000, check)
 
 def play_snd():
+    global music
+    music = True
     pygame.mixer.init()
-    pygame.mixer.music.load("reminder.mp3")
+    pygame.mixer.music.load("Ленинград, Uma2rman - Прасковья.mp3")
     pygame.mixer.music.play()
+
+def stop_music():
+    global music
+    if music:
+        pygame.mixer.music.stop()
+        music = False
+    label.config(text="Установить новое напоминание")
 
 window = Tk()
 window.title("Напоминание")
@@ -57,6 +68,9 @@ label.pack(pady=10)
 
 set_button = Button(text="Установить напоминание", command=set)
 set_button.pack(pady=10)
+
+stop_button = Button(text="Остановить музыку", command=stop_music)
+stop_button.pack(pady=5)
 
 check()
 
